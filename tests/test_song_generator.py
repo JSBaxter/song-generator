a01f850aa184a10031cdd56a2015e8e547fafefa
jsbaxter <james.baxter@avios.com>
2022-06-02 16:49:22 +0100

    Change name of Note class to PitchClass

--- tests/test_song_generator.py
+++ tests/test_song_generator.py
@@ -1,9 +1,28 @@
 from song_generator import __version__
-from song_generator.model
+from song_generator.model import PitchClass
+
+import pytest
 
 
 def test_version():
     assert __version__ == '0.1.0'
 
-def test_create_note_raises_value_error_for_invalid_note():
+def test_create_PitchClass_raises_type_error_for_no_input():
+    with pytest.raises(TypeError) as err:
+        pitch_class = PitchClass()
+
+def test_create_pitch_class_raises_value_error_for_invalid_pitch_class():
+    with pytest.raises(ValueError) as err:
+        pitch_class = PitchClass('H')
+    with pytest.raises(ValueError) as err:
+        pitch_class = PitchClass(13)
+        
+def test_create_pitch_class_assigns_semitone():
+    c = PitchClass('C')
+    assert c.semitone == 0
+    
+    b = PitchClass('B')
+    assert b.semitone == 11
+    d = PitchClass('D')
+    assert d == 2
     
